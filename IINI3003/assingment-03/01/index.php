abacaa469869ed02ae40e3ce8821899d1f2905fd
picccard <eskil.uhlving@gmail.com>
2019-02-06 21:12:35 +0100

    added assignment-03

---
+++ IINI3003/assingment-03/01/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Title:      index.php
+Date:       04.02.2019
+Author:     Eskil Uhlving Larsen
+*/
+?>
+
+<form action="favMovie.php" method="get">
+Favorite Movie?
+<input type="text" name="fav_movie" value=""><br>
+Why? <br>
+<textarea rows="4" cols="30" name="reason"></textarea>
+
+<input type="submit" value="Next">
+</form>
